95e37eb582b7b7ec132db26d80512cb901579f56
Gia <gia@coa.vn>
2018-05-17 16:18:25 +0700

    update explore backend environment

--- settings.js
+++ settings.js
@@ -1,3 +1,5 @@
+export const ENV = process.env.NODE_ENV || 'development'
+
 export const ASSETS = {
   MIS: '0x2d0f3149aedeae16511c3bc72a339a0cf70eed1bd67a909bbc4b66b91875b825',
   KRE: '0x602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7'
@@ -9,7 +11,7 @@ export const ASSET_HASHES = {
 }
 
 export const RPC_CLIENT = {
-  host: '127.0.0.1',
+  host: ENV === 'production' ? '127.0.0.1' : '192.168.1.200',
   port: 10332,
   path: '/',
   strict: true
