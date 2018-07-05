6db17214ae08a4ec8f459c0ab5aadb0cac35ab96
Gia Nguyen <gia@coa.vn>
2018-07-05 10:36:05 +0000

    remove old gui

--- gui.old/src/components/Windows/index.js
+++
@@ -1,9 +0,0 @@
-export default {
-  BlockWindow: require('./BlockWindow').default,
-  DocumentationWindow: require('./DocumentationWindow').default,
-  ExplorerWindow: require('./ExplorerWindow').default,
-  GuideWindow: require('./GuideWindow').default,
-  PaperWalletWindow: require('./PaperWalletWindow').default,
-  TransactionWindow: require('./TransactionWindow').default,
-  WalletWindow: require('./WalletWindow').default
-}
